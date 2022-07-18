41d6350a436d58ca5014f8b08220c1fad7a00fb7
16145 <16145@DESKTOP-J7LAVVV>
2022-07-17 22:38:28 -0400

    Commit Added examples of functions PIL issue discovered and not resolved

--- ImageWork/imageWork/image_functions.py
+++ ImageWork/imageWork/image_functions.py
@@ -3,3 +3,26 @@ Created on Jul 17, 2022
 
 @author: 16145
 '''
+
+from PIL import Image, ImageFilter, ImageDraw, ImageFont
+import os, sys
+import requests
+from io import BytesIO
+
+def load_image( filename ) :
+    try:
+        myimage = Image.open(filename)
+        myimage.load()
+    except:
+        return None
+    
+def save_image( imageObject, outfilename ) :
+    """
+    Save an image to disk
+    :param imageObject: The Image to save
+    :param outfilename: The target file
+    """
+    imageObject.save( outfilename )
+
+
+
--- ImageWork/imageWork/main.py
+++ ImageWork/imageWork/main.py
@@ -4,5 +4,20 @@ Created on Jul 17, 2022
 @author: 16145
 '''
 
+
+from PIL import Image, ImageFilter, ImageDraw, ImageFont
+import os, sys
+import requests
+from io import BytesIO
+
+# This one is in Main.py only
+from image_functions import *
+
+
 if __name__ == '__main__':
-    pass
+    # open an image file. The default path is where this python module is
+    im = Image.open("SiriusAndViolet.jpg")
+    print(im.width, im.height, im.mode, im.format)  # Display some info about the image
+    
+    my_image = load_image("SiriusAndViolet.jpg")
+    my_image.show(my_image)
